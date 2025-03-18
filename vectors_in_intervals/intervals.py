7a40aaaa0b91e73172003ec502f5de35df14bd8f
Marcus Aichmayr <aichmayr.marcus@gmail.com>
2025-03-18 17:09:32 +0100

    add copyright

--- vectors_in_intervals/intervals.py
+++ vectors_in_intervals/intervals.py
@@ -1,5 +1,15 @@
 """Interval classes."""
 
+#############################################################################
+#  Copyright (C) 2025                                                       #
+#          Marcus S. Aichmayr (aichmayr@mathematik.uni-kassel.de)           #
+#                                                                           #
+#  Distributed under the terms of the GNU General Public License (GPL)      #
+#  either version 3, or (at your option) any later version                  #
+#                                                                           #
+#  http://www.gnu.org/licenses/                                             #
+#############################################################################
+
 from __future__ import annotations
 
 from random import getrandbits
