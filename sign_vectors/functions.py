2416e70cff7d86ee31e9f6d60caeef4ae0cda390
Marcus Aichmayr <aichmayr.marcus@gmail.com>
2025-04-22 15:59:54 +0200

    change default of plot_sign_vectors

--- sign_vectors/functions.py
+++ sign_vectors/functions.py
@@ -224,11 +224,10 @@ def deletion(iterable, indices: list[int]) -> set:
     return set(exclude_indices(iterable, indices)(X) for X in iterable)
 
 
-def plot_sign_vectors(iterable, vertex_size: int = 600, figsize: int = 10, aspect_ratio = 4/8):
+def plot_sign_vectors(iterable, vertex_size: int = 600, figsize: int = None, aspect_ratio = None):
     r"""Plot the Hasse Diagram of sign vectors using the conformal relation."""
     Poset((iterable, lambda X, Y: X.conforms(Y))).plot(
         vertex_size=vertex_size,
         element_color="white",
-        cover_style="-",
-        vertex_shape="+",
+        vertex_shape="",
     ).show(figsize=figsize, aspect_ratio=aspect_ratio)
