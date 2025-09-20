5c3e90eb650ee457f383e4e0c7edf07f796ab752
MarcusAichmayr <aichmayr.marcus@gmail.com>
2025-09-20 13:41:33 +0200

    remove closure in favor of lower closure

--- sign_vectors/functions.py
+++ sign_vectors/functions.py
@@ -12,85 +12,8 @@ r"""Functions for working with oriented matroids"""
 
 from sage.combinat.posets.posets import Poset
 
-from sign_vectors import SignVector, sign_vector, zero_sign_vector
+from sign_vectors import SignVector, zero_sign_vector
 
-
-def closure(iterable) -> set[SignVector]:
-    r"""
-    Compute the closure of given sign vectors.
-
-    INPUT:
-
-    - ``iterable`` -- an iterable of sign vectors
-
-    OUTPUT:
-    Return the (lower) closure of ``iterable`` as a set of sign vectors.
-
-    .. NOTE::
-
-       The sign vector :math:`X` is in the closure
-       of a set of sign vectors :math:`W`
-       if there exists :math:`Y \in W` with :math:`X \leq Y`.
-
-    EXAMPLES:
-
-    We consider a list consisting of only one sign vector::
-
-        sage: from sign_vectors import *
-        sage: W = [sign_vector("+-0")]
-        sage: W
-        [(+-0)]
-        sage: closure(W)
-        {(000), (+00), (0-0), (+-0)}
-
-    Now, we consider a list of three sign vectors::
-
-        sage: W = [sign_vector("++-"), sign_vector("-00"), sign_vector("0--")]
-        sage: W
-        [(++-), (-00), (0--)]
-        sage: closure(W)
-        {(000), (-00), (00-), (0+0), (0+-), (+00), (+0-), (++0), (++-), (0-0), (0--)}
-
-    TESTS::
-
-        sage: closure([])
-        set()
-    """
-    if not iterable:
-        return set()
-    for _ in iterable:
-        length = _.length()
-        break
-
-    output = [{zero_sign_vector(length)}]
-    new_elements = set()
-    for i in range(length):
-        X = sign_vector(1 if k == i else 0 for k in range(length))
-        for Z in iterable:
-            if X <= Z:
-                new_elements.add(X)
-                break
-        Y = -X
-        for Z in iterable:
-            if Y <= Z:
-                new_elements.add(Y)
-                break
-    output.append(new_elements)
-    for i in range(1, length + 1):
-        new_elements = set()
-        for X in output[1]:  # X has always |supp(X)| = 1
-            for Y in output[i]:
-                # TODO: utilize that the supports are sorted
-                if len(set(X.support() + Y.support())) == i + 1:
-                    Z = X.compose_harmonious(Y)
-                    if Z not in new_elements and any(Z <= V for V in iterable):
-                        new_elements.add(Z)
-        if not new_elements:
-            break
-        output.append(new_elements)
-        if len(new_elements) == 1:
-            break
-    return set().union(*output)
 
 def lower_closure(iterable) -> set[SignVector]:
     r"""
