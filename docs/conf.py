8a16fd708cc5cb6c909cf0a0f77364c10a0f8421
MarcusAichmayr <aichmayr.marcus@gmail.com>
2026-04-22 22:34:26 +0200

    improve pdf docs

--- docs/conf.py
+++ docs/conf.py
@@ -114,21 +114,24 @@ htmlhelp_basename = 'elementary_vectors_doc'
 # -- Options for LaTeX output ------------------------------------------------
 
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+    "classoptions": ",oneside",
+    "papersize": "a4paper",
+    "pointsize": "11pt",
+    "preamble": r"""
+        \usepackage{amsmath,amssymb,amsfonts}
+        \usepackage{mathtools}
+        \makeatletter
+        \renewenvironment{theindex}
+        {\chapter*{\indexname}
+            \markboth{\indexname}{\indexname}
+            \thispagestyle{plain}
+            \parindent 0pt
+            \parskip 0.3em
+            \let\item\par
+        }
+        {}
+        \makeatother
+    """,
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
