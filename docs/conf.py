c7cf2ebf772823322770f673c75cd63e9d34ebe3
Marcus Aichmayr <aichmayr.marcus@gmail.com>
2026-07-12 15:02:55 +0200

    v2.4

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = u'2026, Marcus S. Aichmayr'
 author = u'Marcus S. Aichmayr'
 
 # The short X.Y version
-version = u'2.3'
+version = u'2.4'
 # The full version, including alpha/beta/rc tags
-release = u'2.3'
+release = u'2.4'
 
 # -- General configuration ---------------------------------------------------
 
